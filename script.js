7a28d8827957fa4c829ec465ef91b03274b0d6ee
Kyle E. Mitchell <kyle@kemitchell.com>
2020-04-19 11:17:48 -0700

    Implement CodeMirror linter

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
+var commonmark = require('commonform-commonmark')
+var critique = require('commonform-critique')
+var lint = require('commonform-lint')
+
 var CodeMirror = require('codemirror')
+require('codemirror/mode/markdown/markdown')
+require('codemirror/addon/lint/lint')
 
 document.addEventListener('DOMContentLoaded', function () {
+  configureLinters()
   addEditor()
   addHelpLinks()
 })
+
+function configureLinters () {
+  CodeMirror.registerHelper('lint', 'markdown', function (text, options) {
+    var from = CodeMirror.Pos(0 /* line */, 0 /* column */)
+    var to = CodeMirror.Pos(0, 0)
+    try {
+      var parsed = commonmark.parse(text)
+    } catch (error) {
+      return [
+        {
+          from: from,
+          to: to,
+          message: 'Invalid Syntax',
+          severity: 'error'
+        }
+      ]
+    }
+    var messages = []
+      .concat(lint(parsed.form))
+      .concat(critique(parsed.form))
+      .map(function (annotation) {
+        return {
+          from: from,
+          to: to,
+          message: annotation.message,
+          severity: annotation.level
+        }
+      })
+    return messages
+  })
+}
 
 function addEditor () {
   var editor = CodeMirror(document.getElementById('main'), {
     autofocus: true,
     spellcheck: true,
     lineWrapping: true,
-    mode: require('codemirror/mode/markdown/markdown')
+    mode: 'markdown',
+    gutters: ['CodeMirror-lint-markers'],
+    lint: true
   })
   editor.setValue('# Welcome')
+  editor.on('changes', function () {
+  })
 }
 
 function addHelpLinks () {
