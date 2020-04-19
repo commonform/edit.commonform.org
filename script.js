bcd67693a0bbec23666bd81efebfcaf764af8149
Kyle E. Mitchell <kyle@kemitchell.com>
2020-04-19 11:49:15 -0700

    Implement .docx downloads

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
+var FileSaver = require('file-saver')
 var commonmark = require('commonform-commonmark')
 var critique = require('commonform-critique')
+var docx = require('commonform-docx')
 var lint = require('commonform-lint')
+
+var numberings = {
+  outline: {
+    label: 'Outline',
+    numbering: require('outline-numbering')
+  },
+  decimal: {
+    label: 'Decimal',
+    numbering: require('decimal-numbering')
+  },
+  ase: {
+    label: 'Agreement, Schedules, Exhibits',
+    numbering: require('agreement-schedules-exhibits-numbering')
+  },
+  rse: {
+    label: 'Resolutions, Schedules, Exhibits',
+    numbering: require('resolutions-schedules-exhibits-numbering')
+  }
+}
 
 var CodeMirror = require('codemirror')
 require('codemirror/mode/markdown/markdown')
 require('codemirror/addon/lint/lint')
+require('codemirror/addon/display/panel')
 
 document.addEventListener('DOMContentLoaded', function () {
   configureLinters()
   addEditor()
+  addPanel()
   addHelpLinks()
 })
 
@@ -44,17 +67,64 @@ function configureLinters () {
 }
 
 function addEditor () {
-  var editor = CodeMirror(document.getElementById('main'), {
-    autofocus: true,
-    spellcheck: true,
-    lineWrapping: true,
-    mode: 'markdown',
-    gutters: ['CodeMirror-lint-markers'],
-    lint: true
+  window.editor = CodeMirror(
+    document.getElementById('editor'),
+    {
+      autofocus: true,
+      spellcheck: true,
+      lineWrapping: true,
+      mode: 'markdown',
+      gutters: ['CodeMirror-lint-markers'],
+      lint: true,
+      value: '# Welcome'
+    }
+  )
+}
+
+function addPanel () {
+  var panel = document.createElement('div')
+  panel.id = 'panel'
+
+  var wordButton = document.createElement('button')
+  wordButton.appendChild(document.createTextNode('Download for Word'))
+  wordButton.addEventListener('click', function () {
+    var options = {
+      centerTitle: false,
+      indentMargins: true,
+      markFilled: true,
+      numbering: numberings[numberingSelect.value].numbering,
+      styles: {
+        alignment: 'left',
+        heading: { italic: true },
+        reference: { italic: true },
+        referenceHeading: { italic: true }
+      }
+    }
+    try {
+      var parsed = commonmark.parse(window.editor.getValue())
+    } catch (error) {
+      console.error(error)
+      return
+    }
+    docx(parsed.form, [], options)
+      .generateAsync({ type: 'blob' })
+      .then(function (blob) {
+        var date = new Date().toISOString()
+        FileSaver.saveAs(blob, 'commonform-' + date + '.docx', true)
+      })
   })
-  editor.setValue('# Welcome')
-  editor.on('changes', function () {
+  panel.appendChild(wordButton)
+
+  var numberingSelect = document.createElement('select')
+  Object.keys(numberings).forEach(function (key) {
+    var option = document.createElement('option')
+    option.value = key
+    option.appendChild(document.createTextNode(numberings[key].label))
+    numberingSelect.appendChild(option)
   })
+  panel.appendChild(numberingSelect)
+
+  window.editor.addPanel(panel, { position: 'after-top', stable: true })
 }
 
 function addHelpLinks () {
